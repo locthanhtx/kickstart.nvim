6fb25dd32c52c76d1f2ad6a34b32044ef02b1d2b
Thanh Vo <thanhvnl@nexlesoft.com>
2024-05-28 13:44:02 +0700

    re structure

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  require 'custom.plugins.nvim-tree',
+  require 'custom.plugins.which-key',
+  require 'custom.plugins.gitsigns',
+  require 'custom.plugins.telescope',
+  require 'custom.plugins.lspconfig',
+  require 'custom.plugins.autoformat',
+  require 'custom.plugins.autocompletion',
+  require 'custom.plugins.nvim-treesitter',
+  require 'custom.plugins.others',
+}
